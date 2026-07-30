5637ea962c0c02951f007e10328e47e015336781
Ashorin Ganiu <ashoringaniu@gmail.com>
2026-07-30 15:04:04 +0100

    Improve footer and update styling

--- mainland-leisure-hotel/src/components/layouts/Footer.tsx
+++ mainland-leisure-hotel/src/components/layouts/Footer.tsx
@@ -1,19 +1,20 @@
 import asdf from "../../assets/Images/mainlogo.jpeg";
 import { MapPin, Phone, Mail } from "lucide-react";
+import { FaFacebookF, FaInstagram, FaTiktok, FaTwitter } from "react-icons/fa";
 import "../../styles/footer.css";
 function Footer() {
   return (
     <section className="footer">
       <div className="foot-lead">
         <div className="foot-head">
-          <div>
-            <div>
+          <div className="foot-head-hero">
+            <div className="div-img">
               <img src={asdf} alt="" className="img" />
             </div>
             <div>
-              <h1>
+              <h2>
                 MAINLAND LEISURE <br /> VIEW HOTEL
-              </h1>
+              </h2>
             </div>
           </div>
           <p>
@@ -23,17 +24,25 @@ function Footer() {
         </div>
 
         <div>
-          <h2>Quick Links</h2>
+          <h3>Quick Links</h3>
           <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Rooms</li>
-            <li>Facilities</li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">Rooms</a>
+            </li>
+            <li>
+              <a href="#">Facilities</a>
+            </li>
           </ul>
         </div>
 
         <div>
-          <h2>Useful Links</h2>
+          <h3>Useful Links</h3>
           <ul>
             <li>
               <a href="#">Gallery</a>
@@ -51,7 +60,7 @@ function Footer() {
         </div>
 
         <div>
-          <h2>Contact Us</h2>
+          <h3>Contact Us</h3>
           <ul>
             <li>
               <Phone /> 0909 734 3543
@@ -61,12 +70,58 @@ function Footer() {
             </li>
             <li>
               <MapPin />{" "}
-              <span>
+              <span className="las">
                 2 Ayilara St. Off Clegg Street,
                 <br /> Suruler,Lagos.
               </span>
             </li>
           </ul>
+        </div>
+      </div>
+
+      <div>
+        <h3>Follow Us</h3>
+
+        <div className="social-icons">
+          <div>
+            <a
+              href="https://facebook.com/yourusername"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebookF size={17} />
+            </a>
+          </div>
+
+          <div>
+            <a
+              href="https://tiktok.com/yourusername"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTiktok size={17} />
+            </a>
+          </div>
+
+          <div>
+            <a
+              href="https://twitter.com/yourusername"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter size={17} />
+            </a>
+          </div>
+
+          <div>
+            <a
+              href="https://instagram.com/yourusername"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram size={17} />
+            </a>
+          </div>
         </div>
       </div>
     </section>
